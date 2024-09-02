ba17fac2f2d40057cc725cc7628f271bee0df6c6
joey <joey_qpb@hotmail.com>
2024-09-02 16:51:18 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EBookComponent } from "./components/e-book/e-book.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EBookComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/e-book/e-book.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './e-book.component.html',
+  styleUrl: './e-book.component.css'
+})
+export class EBookComponent {
+  ebookCoverImage = 'ebook-cover.png'
+
+  downloadEbook() {
+    const ebookUrl = 'Ebook-Test.pdf';
+    const link = document.createElement('a');
+    link.href = ebookUrl;
+    link.target = '_blank';
+    link.download = 'titulo-do-ebook.pdf';
+    link.click();
+  }
+}

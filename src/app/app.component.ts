5240630d733e731207602b063a1e757259bac66d
JoeyQPB <joey_qpb@hotmail.com>
2024-09-11 18:43:04 -0300

    app html/css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { EBookComponent } from "./components/e-book/e-book.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { CommentFormComponent } from "./components/comment-form/comment-form.component";
-import { CommentBoxComponent } from "./components/comment-box/comment-box.component";
 import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { ForumComponent } from "./components/forum/forum.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EBookComponent, NavbarComponent, CommentFormComponent, CommentBoxComponent, AboutUsComponent],
+  imports: [EBookComponent, NavbarComponent, AboutUsComponent, ForumComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

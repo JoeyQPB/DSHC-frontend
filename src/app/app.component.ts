ed33275d8bdb4adfc88a0782c9bac745a6821620
JoeyQPB <joey_qpb@hotmail.com>
2024-09-11 15:47:00 -0300

    add new comments and about us components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EBookComponent } from "./components/e-book/e-book.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CommentFormComponent } from "./components/comment-form/comment-form.component";
+import { CommentBoxComponent } from "./components/comment-box/comment-box.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EBookComponent, NavbarComponent],
+  imports: [RouterOutlet, EBookComponent, NavbarComponent, CommentFormComponent, CommentBoxComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
